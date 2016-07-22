0aba7feba032930856816d47b680f153d1392d1b
Pavel V. Talanov <talanov.pavel@gmail.com>
2016-07-22 16:17:37 +0300

    Test dollars in name
     #KT-12160 Fixed

---
+++ idea/testData/checker/regression/DollarsInName.kt
@@ -0,0 +1,23 @@
+package a.b.c
+
+object `$$$$qwe$asdasd$asdds` {
+    fun `aasdsd$asadsd`() {
+
+    }
+}
+
+fun main() {
+    `$$$$qwe$asdasd$asdds`.`aasdsd$asadsd`()
+}
+
+object `$$Test$Test` {
+    val a = 5
+
+    class `$$$` {
+        class `$$`
+    }
+}
+
+fun test() {
+    `$$Test$Test`.`$$$`.`$$`()
+}
